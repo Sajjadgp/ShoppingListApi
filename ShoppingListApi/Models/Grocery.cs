a21ad88874b1af50150fb1627c119f07d83415c6
sajjadgp <sajjad.tekv.28@gmail.com>
2022-11-08 00:08:37 +0330

    initial commit

---
+++ ShoppingListApi/Models/Grocery.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingListApi.Models;
+
+public class Grocery
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool Purchased { get; set; }
+}
---
+++ ShoppingListApi/Models/ShppingListContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingListApi.Models;
+
+public class ShppingListContext : DbContext
+{
+    public ShppingListContext(DbContextOptions<ShppingListContext> options) :base(options)
+    {
+        
+    }
+    public DbSet<Grocery> Groceries { get; set; }
+}
